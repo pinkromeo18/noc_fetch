eb942143ecd594093c6448233b5db013a05067f7
ピンクロメオ <99705203+pinkromeo18@users.noreply.github.com>
2022-02-28 15:35:53 +0900

    Update noc_fetch.js

--- noc_fetch.js
+++ noc_fetch.js
@@ -1,6 +1,8 @@
 //2022, pinkromeo18, MIT.
 window._fetch=window.fetch;
+var noc_fetch_debug =document.querySelector('script[src$="noc_fetch.js"')?false:true;
 window.fetch = function __fetch__(u,o){
   o=Object.assign({},o, {cache:'no-cache'} )
+  if(noc_fetch_debug) console.log(o);
   return window._fetch(u,o);
 }
